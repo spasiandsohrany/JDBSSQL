15bf9f3184502500365b18eec029952748c495d3
spasiandsohrany <kazakov.nikitta@yandex.ru>
2023-01-24 14:57:18 +0300

    3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,19 +2,16 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //Util util = new Util();
-        Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
-        System.out.println(userDao.getAllUsers());
-        /*userDao.dropUsersTable();
-        userDao.createUsersTable();
-        userDao.saveUser("In", "Ib", (byte) 21);
-        //userDao.cleanUsersTable();*/
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.dropUsersTable();
     }
 }
