58621b3d61c70f5bd2ec5e3fb32e15524fed5607
spasiandsohrany <kazakov.nikitta@yandex.ru>
2023-01-24 15:14:03 +0300

    3

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public UserServiceImpl() {
 
